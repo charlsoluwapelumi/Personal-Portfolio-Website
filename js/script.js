542a4909f4946c97fc83379a8a823e03be276dfe
Sns Charmie <71021496+charlsoluwapelumi@users.noreply.github.com>
2022-10-23 21:02:45 +0100

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,7 @@
+// typing animation
+var typed = new Typed(".typing", {
+    strings:["", "Web Developer", "UI/UX Designer", "Graphics Designer"],
+    typeSpeed:100,
+    BackSpeed:60,
+    loop:true
+})
